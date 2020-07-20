490d0fae6a1ea64400c18a595feb142d29c52928
kizymatzen <kizymatzen@gmail.com>
2020-07-20 11:46:58 -0600

    Updated

---
+++ view/dbo.v_rev_area_yr.sql
@@ -0,0 +1,46 @@
+/********************************************************************************************
+NAME:    [dbo].[v_rev_area_yr]
+PURPOSE: Create the [dbo].[v_rev_area_yr] view
+
+SUPPORT: Kizy Matzenbacher
+         kizymatzen@gmail.com
+
+MODIFICATION LOG:
+Ver   Date        Author    Description
+---   ----------  -------   -----------------------------------------------------------------
+1.0   07/20/2020  KIZYMATZEN   1. Created the view
+
+RUNTIME: 
+1 min
+
+NOTES: 
+(None)
+
+LICENSE: 
+This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+********************************************************************************************/
+USE [DFNB2]
+GO
+
+/****** Object:  View [dbo].[v_rev_area_yr]    Script Date: 7/20/2020 11:44:04 AM ******/
+DROP VIEW [dbo].[v_rev_area_yr]
+GO
+
+/****** Object:  View [dbo].[v_rev_area_yr]    Script Date: 7/20/2020 11:44:04 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[v_rev_area_yr] AS
+SELECT DISTINCT p1.acct_area_id AS 'Area ID', SUM(laf.loan_int_amt) AS 'Interest Revenue', YEAR(laf.loan_open_date) AS Year
+FROM [dbo].[t_loan_amt_fact] AS laf
+INNER JOIN [dbo].[stg_p1] AS p1  ON p1.acct_id = laf.acct_id
+GROUP BY YEAR(laf.loan_open_date), acct_area_id
+GO
+
+
---
+++ view/dbo.v_rev_branch_yr.sql
@@ -0,0 +1,49 @@
+/********************************************************************************************
+NAME:    [dbo].[v_rev_branch_yr]
+PURPOSE: Create the [dbo].[v_rev_branch_yr] view
+
+SUPPORT: Kizy Matzenbacher
+         kizymatzen@gmail.com
+
+MODIFICATION LOG:
+Ver   Date        Author    Description
+---   ----------  -------   -----------------------------------------------------------------
+1.0   07/20/2020  KIZYMATZEN   1. Created the view
+
+RUNTIME: 
+1 min
+
+NOTES: 
+(None)
+
+LICENSE: 
+This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+********************************************************************************************/
+USE [DFNB2]
+GO
+
+/****** Object:  View [dbo].[v_rev_branch_yr]    Script Date: 7/20/2020 11:44:11 AM ******/
+DROP VIEW [dbo].[v_rev_branch_yr]
+GO
+
+/****** Object:  View [dbo].[v_rev_branch_yr]    Script Date: 7/20/2020 11:44:11 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE VIEW [dbo].[v_rev_branch_yr] AS
+SELECT DISTINCT p1.acct_branch_desc AS 'Branch ID', SUM(laf.loan_int_amt) AS 'Interest Revenue', YEAR(laf.loan_open_date) AS Year
+FROM [dbo].[t_loan_amt_fact] AS laf
+INNER JOIN [dbo].[stg_p1] AS p1 ON p1.acct_id = laf.acct_id
+GROUP BY YEAR(laf.loan_open_date), p1.acct_branch_desc
+
+GO
+
+
